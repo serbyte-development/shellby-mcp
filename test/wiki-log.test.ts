3129beab5013f355cc5b1524f170fa50273702d4
Austin1serb <serbaustin@gmail.com>
2026-08-12 11:09:02 -0600

    Fix log order.

--- test/wiki-log.test.ts
+++ test/wiki-log.test.ts
@@ -4,10 +4,17 @@ import test from "node:test";
 
 test("keeps the wiki maintenance log in chronological order", async () => {
 	const log = await readFile(new URL("../wiki/log.md", import.meta.url), "utf8");
-	const dates = [...log.matchAll(/^## \[(\d{4}-\d{2}-\d{2})\]/gm)].map((match) => match[1]);
+	const dates = [...log.matchAll(/^## \[(\d{4}-\d{2}-\d{2})\]/gm)].map((match) => {
+		const date = match[1];
+		assert.ok(date);
+		return date;
+	});
 
-	assert.ok(dates.length > 0, "expected at least one dated wiki log entry");
-	for (let index = 1; index < dates.length; index += 1) {
-		assert.ok(dates[index - 1] <= dates[index], `wiki log entry ${dates[index]} appears after ${dates[index - 1]}`);
+	const [firstDate, ...remainingDates] = dates;
+	assert.ok(firstDate, "expected at least one dated wiki log entry");
+	let previousDate = firstDate;
+	for (const date of remainingDates) {
+		assert.ok(previousDate <= date, `wiki log entry ${date} appears after ${previousDate}`);
+		previousDate = date;
 	}
 });
